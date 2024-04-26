40293027663101ede16bba14e3d721f3bb944f1a
Gabrax <gab-oz@wp.pl>
2024-04-26 14:03:58 +0200

    all STD sorting functions

--- Algorithms/Sorting.cpp
+++ Algorithms/Sorting.cpp
@@ -26,6 +26,63 @@ int main() {
     return 0;
 }
 
+//std::stable_sort: Similar to std::sort, but maintains the relative order of elements with equal values.
+int main() {
+    std::vector<int> numbers = {5, 2, 9, 3, 6};
+
+    // Sort the vector in ascending order
+    std::stable_sort(numbers.begin(), numbers.end());
+
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
+
+//std::partial_sort: Sorts the specified range such that the first N elements are sorted in ascending order.
+int main() {
+    std::vector<int> numbers = {5, 2, 9, 3, 6};
+
+    // Sort the vector in ascending order
+    std::partial_sort(numbers.begin(), numbers.begin() + N, numbers.end());
+
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
+
+//std::partial_sort_copy: Copies and partially sorts elements from the input range to the output range.
+int main() {
+    std::vector<int> numbers = {5, 2, 9, 3, 6};
+
+    // Sort the vector in ascending order
+    std::partial_sort_copy(numbers.begin(), numbers.end(), numbers.begin(), numbers.end());
+
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
+
+//std::nth_element: Rearranges elements in such a way that the element at the N-th position is the element that would be in that position in a sorted sequence.
+int main() {
+    std::vector<int> numbers = {5, 2, 9, 3, 6};
+
+    // Sort the vector in ascending order
+    std::nth_element(numbers.begin(), numbers.begin() + N, numbers.end());
+
+
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
+
 //Bubble Sort (for educational purposes):
 //
 //Bubble sort is a simple sorting algorithm but not very efficient. It repeatedly steps through the list,
