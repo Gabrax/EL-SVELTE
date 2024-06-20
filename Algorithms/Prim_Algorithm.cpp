7ef545161ac2e2648145bbca38f4838bf30db12a
Gabrax <gab-oz@wp.pl>
2024-06-20 13:43:53 +0200

    changed Prim algoritm to Priority Queue

--- Algorithms/Prim_Algorithm.cpp
+++ Algorithms/Prim_Algorithm.cpp
@@ -3,102 +3,83 @@
 // for adjacency matrix representation of the graph
  
 #include <iostream>
-using namespace std;
- 
-// Number of vertices in the graph
-#define V 5
- 
-// A utility function to find the vertex with
-// minimum key value, from the set of vertices
-// not yet included in MST
-int minKey(int key[], bool mstSet[])
-{
-    // Initialize min value
-    int min = INT_MAX, min_index;
- 
-    for (int v = 0; v < V; v++)
-        if (mstSet[v] == false && key[v] < min)
-            min = key[v], min_index = v;
- 
-    return min_index;
-}
- 
-// A utility function to print the
-// constructed MST stored in parent[]
-void printMST(int parent[], int graph[V][V])
-{
-    cout << "Edge \tWeight\n";
-    for (int i = 1; i < V; i++)
-        cout << parent[i] << " - " << i << " \t"
-             << graph[i][parent[i]] << " \n";
-}
- 
-// Function to construct and print MST for
-// a graph represented using adjacency
-// matrix representation
-void primMST(int graph[V][V])
-{
-    // Array to store constructed MST
-    int parent[V];
- 
-    // Key values used to pick minimum weight edge in cut
-    int key[V];
- 
-    // To represent set of vertices included in MST
-    bool mstSet[V];
- 
-    // Initialize all keys as INFINITE
-    for (int i = 0; i < V; i++)
-        key[i] = INT_MAX, mstSet[i] = false;
- 
-    // Always include first 1st vertex in MST.
-    // Make key 0 so that this vertex is picked as first
-    // vertex.
-    key[0] = 0;
-   
-    // First node is always root of MST
-    parent[0] = -1;
- 
-    // The MST will have V vertices
-    for (int count = 0; count < V - 1; count++) {
-         
-        // Pick the minimum key vertex from the
-        // set of vertices not yet included in MST
-        int u = minKey(key, mstSet);
- 
-        // Add the picked vertex to the MST Set
-        mstSet[u] = true;
- 
-        // Update key value and parent index of
-        // the adjacent vertices of the picked vertex.
-        // Consider only those vertices which are not
-        // yet included in MST
-        for (int v = 0; v < V; v++)
- 
-            // graph[u][v] is non zero only for adjacent
-            // vertices of m mstSet[v] is false for vertices
-            // not yet included in MST Update the key only
-            // if graph[u][v] is smaller than key[v]
-            if (graph[u][v] && mstSet[v] == false
-                && graph[u][v] < key[v])
-                parent[v] = u, key[v] = graph[u][v];
+#include <vector>
+#include <limits>
+#include <queue>
+#include <utility>
+
+typedef std::pair<int, int> iPair;  // Definicja pary typu (waga, wierzchołek)
+
+void printMST(const std::vector<int>& parent, const std::vector<std::vector<int>>& graph, int V) {
+    std::cout << "Krawedz\tWaga\n";
+    for (int i = 1; i < V; i++) {
+        std::cout << parent[i] << " - " << i << '\t' << graph[i][parent[i]] << '\n';
     }
- 
-    // Print the constructed MST
-    printMST(parent, graph);
 }
- 
-// Driver's code
-int main()
-{
-    int graph[V][V] = { { 0, 2, 0, 6, 0 },
-                        { 2, 0, 3, 8, 5 },
-                        { 0, 3, 0, 0, 7 },
-                        { 6, 8, 0, 0, 9 },
-                        { 0, 5, 7, 9, 0 } };
- 
-    // Print the solution
-    primMST(graph);
- 
-    return 0;
+
+void PrimAlgo(const std::vector<std::vector<int>>& graph, int V) {
+    std::priority_queue<iPair, std::vector<iPair>, std::greater<iPair>> pq;  // Kopiec minimalny
+
+    int src = 0;  // Startujemy od wierzchołka 0
+
+    std::vector<int> key(V, std::numeric_limits<int>::max());  // Wartości klucza dla wierzchołków
+    std::vector<int> parent(V, -1);  // Tablica rodziców MST
+    std::vector<bool> inMST(V, false);  // Czy wierzchołek jest w MST
+
+    pq.push(std::make_pair(0, src));  // Dodanie źródła do kopca
+    key[src] = 0;  // Wartość klucza źródła ustawiona na 0
+
+    while (!pq.empty()) {
+        int u = pq.top().second;  // Pobranie wierzchołka o najmniejszej wartości klucza
+        pq.pop();
+
+        inMST[u] = true;  // Dodanie wierzchołka do MST
+
+        // Przeszukiwanie wszystkich sąsiadów wierzchołka u
+        for (int v = 0; v < V; v++) {
+            int weight = graph[u][v];  // Waga krawędzi u-v
+
+            if (weight && !inMST[v] && weight < key[v]) {  // Aktualizacja klucza, jeśli waga jest mniejsza
+                key[v] = weight;
+                pq.push(std::make_pair(key[v], v));
+                parent[v] = u;  // Ustawienie rodzica wierzchołka v
+            }
+        }
+    }
+
+    printMST(parent, graph, V);  // Drukowanie wyniku
 }
+
+int main() {
+    int V;
+    std::cout << "Liczba wierzcholkow: ";
+    std::cin >> V;
+
+    std::vector<std::vector<int>> graph(V, std::vector<int>(V));  // Inicjalizacja macierzy sąsiedztwa
+
+    std::cout << "Macierz sasiedztwa:" << '\n';
+    for (int i = 0; i < V; i++) {  // Wczytanie macierzy sąsiedztwa
+        for (int j = 0; j < V; j++) {
+            std::cin >> graph[i][j];
+        }
+    }
+
+    PrimAlgo(graph, V);  // Wywołanie algorytmu Prima
+}
+
+/* graf 1
+   0 2 0 6 0
+   2 0 3 8 5
+   0 3 0 0 7
+   6 8 0 0 9    
+   0 5 7 9 0
+*/
+
+/* graf 2
+    0 4 0 6 0 0
+    4 0 7 0 11 0
+    0 7 0 0 0 4
+    6 0 0 0 2 0
+    0 11 0 2 0 1
+    0 0 4 0 1 0
+*/
