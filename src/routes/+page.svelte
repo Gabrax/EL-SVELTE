cdea47a2d816ef09f6866c9ee6c44eafae42675a
Gabrax <gab-oz@wp.pl>
2025-03-24 20:28:42 +0100

    established supabase session refreshing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="hero min-h-screen bg-base-300">
+    <div class="hero-content">
+        <div class="max-w-md text-center">
+            <h1 class="text-white font-bold text-4xl">Welcome to Events Calendar</h1>
+            <p class="py-6">Track your favourite events!</p>
+            <a href="/login" class="btn btn-primary">Create An Account</a>
+        </div>
+    </div>
+</div>
